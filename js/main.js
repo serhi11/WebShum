7107e9a30799ec11cd3fb1c6a991f0896be2a149
Serhi Dorotuch <shumjachi@gmail.com>
2018-11-08 13:16:29 +0300

    df

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
+/**
+ * 01 - ЕФЕКТ ДРУКУ ТЕКСТУ
+ * 02 - ПРЕЛОАДЕР
+ */
+
+var globalPreloadEnd = 0;
+
 var webshum = (function() {
 
-	// ЕФЕКТ ДРУКУ ТЕКСТУ
+	// 01 - ЕФЕКТ ДРУКУ ТЕКСТУ
 	function EfectPrintText(obj) {
 		var elem = document.querySelector(obj.elem),
 			text = elem.innerHTML,
@@ -18,8 +25,20 @@ var webshum = (function() {
 	}
 
 	var logoTextSlogan = new EfectPrintText({
-		elem: '.slogan',
+		elem: '.loader',
 		speed: 100,
-		paused: 1000
+		paused: 0
 	});
+
+	// 02 - ПРЕЛОАДЕР
+	document.body.onload = function() {
+		setTimeout(function() {
+			var preloader = document.getElementById('page-preloader');
+
+			if (!preloader.classList.contains('done')) {
+				preloader.classList.add('done');
+				document.querySelector('.logo').classList.add('active-anim');
+			}
+		}, 5000);
+	}
 })();
